d09886ee4490dc550cf962b3d4492256ac5f13d1
Chris McClure <chrismcclure2010@gmail.com>
2019-05-14 22:30:23 +0300

    made flyer a modal image that can be opened

--- public_html/util.js
+++ public_html/util.js
@@ -28,3 +28,20 @@ function displayDiv(id) {
         $('#' + id).html(data).css("display", "block");
     });
 }
+
+var modal = document.getElementById("myModal");
+
+var img = document.getElementById("flyer");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function(){
+    modal.style.display = "none";
+}
